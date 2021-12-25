5fc99d2297afc27cdab092f961ccb1ce64f0a424
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-12-24 19:42:01 -0800

    Start Express Server

--- fullExample.js
+++ fullExample.js
@@ -13,6 +13,9 @@ var app = express();
 console.log("Hello World");
 
 // (2) Start a Working Express Server
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
 
 // (3) Serve an HTML file
 
