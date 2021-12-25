9674c03a63467be759f0668de79755981a0dc18d
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-12-24 19:19:33 -0800

    initial commit

---
+++ fullExample.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var app = express();
+
+/* ============================================ 
+(7)  Mount the Logger here
+=============================================== */
+
+/* ============================================ 
+(11)  Mount the body-parser here
+=============================================== */
+
+// (1) Meet the node console.
+
+// (2) Start a Working Express Server
+
+// (3) Serve an HTML file
+
+// (4) Serve Static Assets
+
+// (5) Serve JSON on a Specific Route
+
+// (6) Use the .env File
+
+// (7) Implement a Root-Level Request Logger Middleware (a logger)
+
+// (8) Chain Middleware to Create a Time Server
+
+// (9)  Get Route Parameter Input from the Client
+
+// (10) Get Query Parameter Input from the Client
+
+// (11) Use body-parser to Parse POST Requests
+
+// (12) Get Data from POST Requests
+
+/*
+===========================================================
+                DO NOT EDIT BELOW THIS LINE
+===========================================================
+*/
+
+module.exports = app;
